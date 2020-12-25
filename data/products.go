335560e9933471ebfe35e576317d25bf5d8411d5
Serj1c <sergej.a.isaev@gmail.com>
2020-12-26 00:17:21 +0300

    GET productList + POST product added

--- data/products.go
+++ data/products.go
@@ -1,19 +1,55 @@
 package data
 
 import (
+	"encoding/json"
+	"io"
 	"time"
 )
 
 // Product defines the structure for an API product
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       float32
-	SKU         string
-	CreatedOn   string
-	UpdatedOn   string
-	DeletedOn   string
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+// Products is a collection of Product
+type Products []*Product
+
+// ToJSON serializes the contents of the collection to JSON
+// NewEncoder provides better performance than json.Unmarshal
+// as it does not have to buffer the output into an in memmory slice of bytes
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+// FromJSON ...
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
+// GetProducts returns product list
+func GetProducts() Products {
+	return productList
+}
+
+// AddProduct ...
+func AddProduct(p *Product) {
+	p.ID = generateNextID()
+	productList = append(productList, p)
+}
+
+// creates id for a new product to be added to the store
+func generateNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 var productList = []*Product{
--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+
+	"github.com/Serj1c/microservices/data"
 )
 
 // Products ...
@@ -10,11 +12,51 @@ type Products struct {
 	l *log.Logger
 }
 
-// NewProducts ...
+// NewProducts creates a products handler with given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and satisfies the http.Handler interface
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle the request for a list of products
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
 
+	// handle create a new product
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	// catch the rest: if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET products")
+
+	// fetch the data from the data store
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST product")
+
+	prod := &data.Product{} // prod is a new product to be added
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+	data.AddProduct(prod)
 }
